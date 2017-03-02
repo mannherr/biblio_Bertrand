50d295c855c5ff02560d5c02667b6f7344f1b394
AFPA <AFPA@192.168.101.135>
2017-03-02 12:50:45 +0100

    Veersion 1.2

--- Bibliotheques/src/metier/EmpruntArchive.java
+++ Bibliotheques/src/metier/EmpruntArchive.java
@@ -3,38 +3,39 @@ package metier;
 import java.util.Date;
 
 public class EmpruntArchive {
-	
+
 	private Date dateRestitutionEff;
 	private Date dateEmprunt;
 	private Utilisateur emprunteur = new Utilisateur();
 	private Exemplaire exemplaire = new Exemplaire();
-	
-	
-	
+
+
+
+
 	public Date getDateRestitutionEff() {
 		return dateRestitutionEff;
 	}
-	
+
 	public void setDateRestitutionEff(Date dateRestitutionEff) {
 		this.dateRestitutionEff = dateRestitutionEff;
 	}
-	
+
 	public Date getDateEmprunt() {
 		return dateEmprunt;
 	}
-	
+
 	public void setDateEmprunt(Date dateEmprunt) {
 		this.dateEmprunt = dateEmprunt;
 	}
-	
+
 	public Utilisateur getEmprunteur() {
 		return emprunteur;
 	}
-	
+
 	public void setEmprunteur(Utilisateur emprunteur) {
 		this.emprunteur = emprunteur;
 	}
-	
+
 	public Exemplaire getExemplaire() {
 		return exemplaire;
 	}
@@ -46,20 +47,20 @@ public class EmpruntArchive {
 	public EmpruntArchive(){
 		this(new Date(), new Date(0));
 	}
-	
+
 	public EmpruntArchive(Date dateRestitution, Date dateEmprunt){
 		this.setDateRestitutionEff(dateRestitution);
-		this.setDateEmprunt(dateEmprunt);		
+		this.setDateEmprunt(dateEmprunt);
 	}
-	
+
 	public static void main (String [] args){
-		
-		
+
+
 		EmpruntArchive ea = new EmpruntArchive () ;
-		ea.getEmprunteur().
-		
-		
+
+
+
 	} //main
-	
-		
+
+
 } //class>
